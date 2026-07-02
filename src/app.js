de2a964103f55459732a1c3e98e66828bc282987
Zain-The-Coder <zain015976@gmail.com>
2026-07-02 12:33:50 +0500

    cors path added

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://apex-wallet-frontend.vercel.app/'
+        'https://apex-wallet-frontend.vercel.app'
     ],
     credentials: true 
 }));
